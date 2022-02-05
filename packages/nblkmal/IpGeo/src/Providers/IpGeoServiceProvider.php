cb5ea4f7ab09b7c89192ca3b1afd4c3306637da8
nblkmal <nblkmal@gmail.com>
2022-02-05 13:26:23 +0800

    add service provider

---
+++ packages/nblkmal/IpGeo/src/Providers/IpGeoServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nblkmal\IpGeo\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class IpGeoServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+}
